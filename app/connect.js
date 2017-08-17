54cdc4f166ba6cb14f8f463943132244c2367686
稻草人 <462001808@qq.com>
2017-08-17 19:25:30 +0800

    d

--- app/connect.js
+++ app/connect.js
@@ -1,13 +1,15 @@
 // 数据引入
 var processPreview = require('./mock/process__preview.js')
+var pop = require('./mock/pop.js')
 
 
 // 映射
 var typeObj = {// 数据和key值对应
-    whh:processPreview
+    whh:processPreview,
+    pops:pop
 }
 
 module.exports = function(type){// 发送返回数据
-    var data = {data:typeObj[type]};
+    var data = typeObj[type]
     return data
 }
