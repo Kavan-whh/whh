f10b1ecc171f057a700057353cf8b340aab732d4
稻草人 <462001808@qq.com>
2017-08-19 11:47:57 +0800

    加入clis数据

--- app/connect.js
+++ app/connect.js
@@ -1,9 +1,10 @@
 // 数据引入
 var processPreview = require('./mock/process__preview.js')
 var pop = require('./mock/pop.js')
+var clisData = require('./mock/else.js')
 
 
-var allData = Object.assign(processPreview,pop)
+var allData = Object.assign(processPreview,pop,clisData)
 // 映射
 var typeObj = {// 数据和key值对应
     whh:processPreview,
---
+++ app/mock/else.js
@@ -0,0 +1,320 @@
+var data ={
+    "banner": {
+      "d": [
+        {
+          "id": 1,
+          "src": "images/banner.png"
+        },
+        {
+          "id": 2,
+          "src": "images/banner.png"
+        },
+        {
+          "id": 3,
+          "src": "images/banner.png"
+        }
+      ]
+    },
+    "seaverList":{
+        "d":[ {
+                "id": 1,
+                "name": "校园攻略",
+                "detail": "欢迎新生加入北京航空航天大学",
+                "logo": "images/school.png",
+                "type": "新生流程"
+            },
+            {
+                "id": 2,
+                "name": "校园攻略1",
+                "detail": "欢迎新生加入北京航空航天大学",
+                "logo": "images/school.png",
+                "type": "新生流程"
+            },
+            {
+                "id": 3,
+                "name": "校园攻略2",
+                "detail": "欢迎新生加入北京航空航天大学",
+                "logo": "images/school.png",
+                "type": "新生流程"
+            },
+            {
+                "id": 4,
+                "name": "校园攻略3",
+                "detail": "欢迎新生加入北京航空航天大学",
+                "logo": "images/school.png",
+                "type": "新生流程"
+            },
+            {
+                "id": 5,
+                "name": "校园攻略4",
+                "detail": "欢迎新生加入北京航空航天大学",
+                "logo": "images/school.png",
+                "type": "新生流程"
+            },
+            {
+                "id": 6,
+                "name": "校园攻略5",
+                "detail": "欢迎新生加入北京航空航天大学",
+                "logo": "images/school.png",
+                "type": "新生流程"
+            },
+            {
+                "id": 7,
+                "name": "校园攻略6",
+                "detail": "欢迎新生加入北京航空航天大学",
+                "logo": "images/school.png",
+                "type": "新生流程"
+            },
+            {
+                "id": 8,
+                "name": "校园攻略7",
+                "detail": "欢迎新生加入北京航空航天大学",
+                "logo": "images/school.png",
+                "type": "新生流程"
+            },
+            {
+                "id": 9,
+                "name": "校园攻略8",
+                "detail": "欢迎新生加入北京航空航天大学",
+                "logo": "images/school.png",
+                "type": "新生流程"
+            },
+            {
+                "id": 10,
+                "name": "校园攻略9",
+                "detail": "欢迎新生加入北京航空航天大学",
+                "logo": "images/school.png",
+                "type": "新生流程"
+            },
+            {
+                "id": 11,
+                "name": "校园攻略10",
+                "detail": "欢迎新生加入北京航空航天大学",
+                "logo": "images/school.png",
+                "type": "新生流程"
+            }
+  
+  
+        ]
+    },
+    "serviceObj":{
+      "d":[
+        {
+          "id": 1,
+          "name": "未知"
+        }, {
+          "id": 2,
+          "name": "专科生"
+        }, {
+          "id": 3,
+          "name": "本科生"
+        }, {
+          "id": 4,
+          "name": "研究生"
+        }
+      ]
+  },
+  "item":{
+      "d":{
+          "name":"事项名",
+          "explain":"事项说明",
+          "detail":"事项描述",
+          "logo":"图片",
+          "eventSourceKey":"fhgopsdangkhahgsafjds",
+          "bpmn_id":"1001",
+          "form":{
+              "1":"表单1",
+              "3":"表单2"
+          },
+          "attachment":{
+              "1":"附件1",
+              "3":"附件3"
+          },
+          "pic":"xml格式的流程图片",
+          "department":{
+              "4":"部门1",
+              "5":"部门3"
+          },
+          "object":{
+              "1":"学生",
+              "2":"研究生"
+          },
+          "archive":{
+              "1":"归档分类1"
+          },
+          "usable":{
+              "1":"冯恒俊",
+              "10":"财务部",
+              "100":"xx小组"
+          }
+      }
+  },
+    "team":[
+         {
+             "id":52,
+              "name":"小组1"
+          },
+          {
+              "id":51,
+              "name":"小组2"
+          }
+      ],
+    "job": {
+          "财务部": [
+              {
+                  "id": 1,
+                  "name": "出差报销流程"
+              }, {
+                  "id": 2,
+                  "name": "补助报销流程"
+              }
+          ],
+          "教务处": [
+              {
+                  "id": 1,
+                  "name": "新生报道流程"
+              }, {
+                  "id": 2,
+                  "name": "毕业设计流程"
+              }
+          ],
+          "回收站": [
+  
+          ]
+     },
+     "form": {
+          "科研类": [
+              {
+                  "id": 1,
+                  "name": "出差报销流程"
+              }, {
+                  "id": 2,
+                  "name": "补助报销流程"
+              }
+          ],
+          "教学类": [
+              {
+                  "id": 1,
+                  "name": "新生报道流程"
+              }, {
+                  "id": 2,
+                  "name": "毕业设计流程"
+              }
+          ],
+          "回收站": [
+  
+          ],
+          "草稿箱": [
+  
+          ]
+     },
+    "depMsg":{
+      "e":0,
+      "m":"操作成功",
+      "d":{
+          "id":280,
+          "type":"department",
+          "name":"测试部门",
+          "created":"2017-08-16 10:44:10",
+          "creator":0,
+          "sn":"",
+          "pid":1,
+          "sort":1,
+          "pname":"开发部1",
+          "relation":[
+              {
+                  "id":6,
+                  "departid":280,
+                  "relation_id":3,
+                  "member":{
+                      "uid":"3",
+                      "username":"周克华"
+                  }
+              },
+              {
+                  "id":5,
+                  "departid":280,
+                  "relation_id":2,
+                  "member":{
+                      "uid":"3",
+                      "username":"啊啊啊l"
+                  }
+              },
+              {
+                  "id":4,
+                  "departid":280,
+                  "relation_id":1,
+                  "member":{
+                      "uid":"3",
+                      "username":"哈哈哈"
+                  }
+              }
+          ],
+          "job":[
+              {
+                  "id":3,
+                  "type":"department",
+                  "name":"",
+                  "created":"2017-08-15 11:40:05",
+                  "creator":0,
+                  "jobid":2,
+                  "departid":280,
+                  "jobname":"财务核算"
+              }
+          ]
+      }
+  },
+  "list":[
+          {
+              "uid":62,
+              "name":"admin1",
+              "sex":"女",
+              "username":"admin1",
+              "number":"admin1",
+              "identity":"校本部教职工",
+              "department":[
+                  {
+                      "id":5,
+                      "name":"教学部"
+                  }
+              ],
+              "mobile":"13012345678",
+              "email":"657026170@qq.com"
+          },
+          {
+              "uid":61,
+              "name":"admin",
+              "sex":"男",
+              "username":"admin",
+              "number":"admin",
+              "identity":"校本部教职工",
+              "department":[
+                  {
+                      "id":4,
+                      "name":"教学部"
+                  }
+              ],
+              "mobile":"13012345678",
+              "email":"657026170@qq.com"
+      }
+  ],
+    "comments": [{
+      "id": 1,
+      "body": "some comment",
+      "postId": 1
+    }],
+    "comments": [
+      {
+        "id": 1,
+        "body": "some comment",
+        "postId": 1
+      }
+    ],
+    "profile": {
+      "id": "4",
+      "whh": "sd"
+    }
+  }
+  
+
+  module.exports = data
